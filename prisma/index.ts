6c1ebdad75eb359fc9f3777006fff84b18ad70e6
DnyaneshwarKalwale <prembkalwale@gmail.com>
2025-06-24 19:38:22 +0530

    api added

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,17 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-  return new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+  return new PrismaClient();
 };
 
 declare global {
-  var __prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-const prisma = globalThis.__prisma ?? prismaClientSingleton();
+const prisma = globalThis.prisma ?? prismaClientSingleton();
 
 export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalThis.__prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;

5f19c9590f7ad7e258a87feca8a206c04cc60c62
DnyaneshwarKalwale <prembkalwale@gmail.com>
2025-06-25 12:16:09 +0530

    deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
@@ -9,6 +11,20 @@ const nextConfig = {
   },
   experimental: {
     instrumentationHook: true,
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Ensure case sensitivity doesn't cause issues
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve('./src'),
+      '@/components': path.resolve('./src/components'),
+      '@/lib': path.resolve('./src/lib'),
+      '@/store': path.resolve('./src/store'),
+      '@/app': path.resolve('./src/app'),
+      '@/contexts': path.resolve('./src/contexts'),
+      '@apiUtils': path.resolve('./src/apiUtils'),
+    }
+    return config
   },
 };
 

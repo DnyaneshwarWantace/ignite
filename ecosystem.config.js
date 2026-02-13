185f6c8c8df028db959255a71b2c102fed2008be
DnyaneshwarWantace <dnyaneshwar@wantace.com>
2026-02-13 20:28:44 +0530

    new

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,6 +12,10 @@ module.exports = {
       NODE_ENV: 'production',
       PORT: 3001
     },
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 3001
+    },
     error_file: './logs/err.log',
     out_file: './logs/out.log',
     log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
